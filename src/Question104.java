9f76a7e332e3bcbddf263efa4b0055362001dfb6
zhangyi <810469047@qq.com>
2019-10-23 08:33:00 +0800

    2019.10.22

---
+++ src/Question104.java
@@ -0,0 +1,39 @@
+/**
+ * @author Struggle
+ * @date Created in 10:30 2019/10/22
+ * description 二叉树的高度
+ **/
+public class Question104 {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public static void main(String[] args) {
+        Question104 q = new Question104();
+        TreeNode root = new TreeNode(3);
+        TreeNode a = new TreeNode(9);
+        TreeNode b = new TreeNode(20);
+        TreeNode c = new TreeNode(15);
+        TreeNode d = new TreeNode(7);
+        root.left = a;
+        root.right = b;
+        b.left = c;
+        b.right = d;
+        System.out.println(q.maxDepth(root));
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
---
+++ src/Question88.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+/**
+ * @author Struggle
+ * @date Created in 10:23 2019/10/22
+ * description 将两个有序的数组合并,数组2合并到数组1中
+ **/
+public class Question88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 || j >= 0) {
+            if (i >= 0 && j >= 0) {
+                nums1[k--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+            } else if (i >= 0) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Question88 q = new Question88();
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        q.merge(nums1, 3, nums2, 3);
+        System.out.println(Arrays.toString(nums1));
+    }
+}
