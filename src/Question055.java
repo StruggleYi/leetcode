b93eaf32615bc746855dd8aef199c10888fa5134
zhangyi <810469047@qq.com>
2019-11-04 17:00:13 +0800

    2019.11.04

---
+++ src/Question055.java
@@ -0,0 +1,66 @@
+/**
+ * @author Struggle
+ * @date Created in 14:57 2019/11/4
+ * description Jump Game 给定一组非负的数字, 数字代表最大跳跃的步数, 判断能否成功到达最后一格
+ * path:  https://leetcode.com/problems/jump-game/description/
+ * level: medium
+ **/
+public class Question055 {
+    /**
+     * 个人做法, 采用数组统计哪些位置能够访问
+     * 遍历数组, 即可统计出是否可以访问到最后一个位置
+     * 时间复杂度偏高
+     */
+    public boolean canJump(int[] nums) {
+        boolean[] flag = new boolean[nums.length];
+        flag[0] = true;
+        // 这里加一个判断, 看是否能到i位置
+        for (int i = 0; i < nums.length && flag[i]; i++) {
+            int k = nums[i];
+            int j = i;
+            while (j <= i + k && j < nums.length) {
+                flag[j++] = true;
+            }
+            if (flag[nums.length - 1]) {
+                return true;
+            }
+        }
+        return flag[nums.length - 1];
+    }
+
+    /**
+     * 方法一参照方法二思路进行更新, 去掉用来记录能到达位置的数组, 直接记录能够到达的最远的位置
+     */
+    public boolean canJump3(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length && i <= max; i++) {
+            if (i + nums[i] >= max){
+                max = i + nums[i];
+            }
+        }
+        return max >= nums.length - 1;
+    }
+
+    /**
+     * 参考的简易做法：直接从最后一个节点出发, 往前推算
+     * lastPos 用来记录当前能够到达的节点位置
+     * i + nums[i] >= lastPost 则说明从i节点出发可以到达lastPos位置, 更新位置i
+     * 看最终能否到达起点0, 相比于上面解法, 减少了遍历记录能够到达的点的重复过程
+     */
+    public boolean canJump2(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = lastPos - 1; i >= 0; i--) {
+            if ((i + nums[i]) >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+
+
+    public static void main(String[] args) {
+        Question055 q = new Question055();
+        int[] nums = {2, 0};
+        System.out.println(q.canJump3(nums));
+    }
+}
---
+++ src/Question056.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+/**
+ * @author Struggle
+ * @date Created in 15:34 2019/11/4
+ * description Merge Intervals 给定许多区间(eg:[1,2]), 将重复区间合并
+ * note: 先对数组进行排序, 然后两两求交集
+ * path: https://leetcode.com/problems/merge-intervals/description/
+ * level: medium
+ **/
+public class Question056 {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0){
+            return new int[0][];
+        }
+        Arrays.sort(intervals, (o1, o2) -> {
+            if (o1[0] > o2[0]) {
+                return 1;
+            } else if (o1[0] == o2[0]) {
+                return 0;
+            } else {
+                return -1;
+            }
+        });
+        int i = 0;
+        for (int j = 1; j < intervals.length; j++) {
+            if (intervals[i][1] >= intervals[j][0]) {
+                intervals[i][1] = Math.max(intervals[j][1], intervals[i][1]);
+            }else {
+                i++;
+                intervals[i][0] = intervals[j][0];
+                intervals[i][1] = intervals[j][1];
+            }
+        }
+        return Arrays.copyOfRange(intervals, 0, i + 1);
+    }
+
+    public static void main(String[] args) {
+        Question056 q = new Question056();
+        int[][] nums = {};
+        System.out.println(Arrays.deepToString(q.merge(nums)));
+    }
+}
