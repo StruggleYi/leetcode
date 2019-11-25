377e04a1126fd65e4b8b80b6f4fd5e38dbfe6f35
zhangyi <810469047@qq.com>
2019-11-25 17:36:58 +0800

    2019.11.25

---
+++ src/Question460.java
@@ -0,0 +1,9 @@
+/**
+ * @author Struggle
+ * @date Created in 17:00 2019/11/25
+ * description LFU Cache 最近最不常用置换, 缓存容量如果满了, 将最近最不常用的移除  时间复杂度o(1)
+ * path: https://leetcode.com/problems/lfu-cache/
+ * level: hard
+ **/
+public class Question460 {
+}
