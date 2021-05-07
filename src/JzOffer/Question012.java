dbe438ac2ec4c4e1864f11add181353874bdeb6a
StruggleYi <18171718116@163.com>
2021-05-07 22:49:20 +0800

    2021.5.7

---
+++ src/JzOffer/Question012.java
@@ -0,0 +1,43 @@
+package JzOffer;
+
+/**
+ * @author Struggle
+ * @date Created in 22:06 2021/5/7
+ * description 矩阵中的路径 给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false
+ * node:
+ * path: https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof/
+ * level: medium
+ **/
+public class Question012 {
+    public boolean exist(char[][] board, String word) {
+        char[] words = word.toCharArray();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, words, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    boolean dfs(char[][] board, char[] word, int i, int j, int k) {
+        if (i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) {
+            return false;
+        }
+        if (k == word.length - 1) {
+            return true;
+        }
+        board[i][j] = '\0';
+        boolean res = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i - 1, j, k + 1) ||
+                dfs(board, word, i, j + 1, k + 1) || dfs(board, word, i, j - 1, k + 1);
+        board[i][j] = word[k];
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Question012 q = new Question012();
+        char[][] board = {{'a'}};
+        System.out.println(q.exist(board, "b"));
+    }
+}
