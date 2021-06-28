aa0b4478e34e316a3a963bc5c71284a1adbd830f
StruggleYi <18171718116@163.com>
2021-06-28 22:41:19 +0800

    2021.6.28

---
+++ src/Question815.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+/**
+ * @author Struggle
+ * @date Created in 21:33 2021/6/28
+ * description 公交路线
+ * node:
+ * path: https://leetcode-cn.com/problems/bus-routes/
+ * level: hard
+ **/
+public class Question815 {
+
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+        if (source == target) {
+            return 0;
+        }
+
+        int n = routes.length;
+        boolean[][] edge = new boolean[n][n];
+        Map<Integer, List<Integer>> rec = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for (int site : routes[i]) {
+                List<Integer> list = rec.getOrDefault(site, new ArrayList<>());
+                for (int j : list) {
+                    edge[i][j] = edge[j][i] = true;
+                }
+                list.add(i);
+                rec.put(site, list);
+            }
+        }
+
+        int[] dis = new int[n];
+        Arrays.fill(dis, -1);
+        Queue<Integer> que = new LinkedList<>();
+        for (int bus : rec.getOrDefault(source, new ArrayList<>())) {
+            dis[bus] = 1;
+            que.offer(bus);
+        }
+        while (!que.isEmpty()) {
+            int x = que.poll();
+            for (int y = 0; y < n; y++) {
+                if (edge[x][y] && dis[y] == -1) {
+                    dis[y] = dis[x] + 1;
+                    que.offer(y);
+                }
+            }
+        }
+
+        int ret = Integer.MAX_VALUE;
+        for (int bus : rec.getOrDefault(target, new ArrayList<>())) {
+            if (dis[bus] != -1) {
+                ret = Math.min(ret, dis[bus]);
+            }
+        }
+        return ret == Integer.MAX_VALUE ? -1 : ret;
+    }
+
+
+    public static void main(String[] args) {
+        Question815 q = new Question815();
+        int[][] routes = {{1, 2, 7}, {3, 6, 7}};
+        System.out.println(q.numBusesToDestination(routes, 1, 6));
+    }
+}
