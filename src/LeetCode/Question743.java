12ca64b694a1d31baf857362ec5bf97e5cf7439a
StruggleYi <18171718116@163.com>
2021-08-21 23:20:14 +0800

    2021.8.2

--- src/LeetCode/Question743.java
+++ src/LeetCode/Question743.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 /**
  * @author Struggle
- * @date Created in 23:17 2021/8/21
+ * @date Created in 23:17 2021/8/2
  * description 网络延迟时间 有 n 个网络节点，标记为 1 到 n。
  * 给你一个列表 times，表示信号经过 有向 边的传递时间。 times[i] = (ui, vi, wi)，其中 ui 是源节点，vi 是目标节点， wi 是一个信号从源节点传递到目标节点的时间。
  * 现在，从某个节点 K 发出一个信号。需要多久才能使所有节点都收到信号
