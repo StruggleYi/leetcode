bdd2d255759f8e031bd3ba00c5dd5f5b4ccd3c5e
StruggleYi <18171718116@163.com>
2021-08-21 23:21:18 +0800

    2021.8.3

---
+++ src/LeetCode/Question581.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+/**
+ * @author Struggle
+ * @date Created in 23:20 2021/8/3
+ * description 最短无序连续子数组  给你一个整数数组 nums ，你需要找出一个 连续子数组 ，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序
+ * node:
+ * path: https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray/
+ * level: medium
+ **/
+public class Question581 {
+    public int findUnsortedSubarray(int[] nums) {
+        int n = nums.length;
+        int maxn = Integer.MIN_VALUE, right = -1;
+        int minn = Integer.MAX_VALUE, left = -1;
+        for (int i = 0; i < n; i++) {
+            if (maxn > nums[i]) {
+                right = i;
+            } else {
+                maxn = nums[i];
+            }
+            if (minn < nums[n - i - 1]) {
+                left = n - i - 1;
+            } else {
+                minn = nums[n - i - 1];
+            }
+        }
+        return right == -1 ? 0 : right - left + 1;
+    }
+}
